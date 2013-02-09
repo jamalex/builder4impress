3aa873c38a6ade7a4aa5d1edd2b417d19dbc5f89
Jamie Alexandre <jamalex@gmail.com>
2013-02-09 12:54:19 -0800

    Removed trailing whitespace.

--- js/builder.js
+++ js/builder.js
@@ -33,7 +33,7 @@ Builder=(function(){
   $menu,$controls,$impress,$overview;
 
   handlers.move=function(x,y){
-    
+
     var v=fixVector(x,y);
 
     state.data.x = (state.data.x)? (state.data.x)+v.x : v.x;
@@ -46,16 +46,16 @@ Builder=(function(){
     console.log(state.rotate);
     state.data.rotate-= -x*config.rotateStep;
   };
-  
+
 
   function init(conf){
     config=$.extend(config,conf);
-    
+
     if(config.setTransformationCallback){
       config.setTransformationCallback(function(x){
         // guess what, it indicates slide change too :)
         $controls.hide();
-        
+
         //setting pu movement scale
         config.visualScaling=x.scale;
         console.log(x.scale);
@@ -65,10 +65,10 @@ Builder=(function(){
       //I don't see why I should need translation right now, but who knows...
       })
     }
-    
+
     $impress=$('#impress');
     $overview=$('#overview');
-    
+
     $menu=$('<div></div>').addClass('builder-main');
     $('<div></div>').addClass('builder-bt bt-add').appendTo($menu).text('Add new').on('click',addSlide);
     $('<div></div>').addClass('builder-bt bt-overview').appendTo($menu).text('Overview').on('click',function(){
@@ -76,22 +76,22 @@ Builder=(function(){
     });
     $('<div></div>').addClass('builder-bt bt-download').appendTo($menu).text('Get file').on('click',downloadResults);
     $('<div></div>').addClass('builder-bt bt-download').appendTo($menu).text('style.css').on('click',downloadStyle);
-    
-    
+
+
     $menu.appendTo('body');
-    
-    
+
+
     $controls=$('<div></div>').addClass('builder-controls').hide();
-    
+
     $('<div></div>').addClass('bt-move').attr('title','Move').data('func','move').appendTo($controls);
     $('<div></div>').addClass('bt-rotate').attr('title','Rotate').data('func','rotate').appendTo($controls);
     $('<div></div>').addClass('bt-scale').attr('title','Scale').data('func','scale').appendTo($controls);
-    
+
     $('<span></span>').addClass('builder-bt').text('Edit').appendTo($controls).click(editContents);
     $('<span></span>').addClass('builder-bt').text('Wrap').appendTo($controls).click(wrapContents);
-    
+
     var showTimer;
-    
+
     $controls.appendTo('body').on('mousedown','div',function(e){
       e.preventDefault();
       mouse.activeFunction=handlers[$(this).data('func')];
@@ -107,7 +107,7 @@ Builder=(function(){
       mouse.activeFunction=false;
       $(document).off('mousemove.handler1');
     });
-    
+
     $('body').on('mouseenter','.step',function(){
       var $t=$(this);
       showTimer=setTimeout(function(){
@@ -122,21 +122,21 @@ Builder=(function(){
       //not showing when not staying
       clearTimeout($(this).data('showTimer'));
     });
-    
+
     $(window).on('beforeunload',function(){ return 'All changes will be lost'; });
-    
+
     config['goto']('start');
-    
-    
+
+
   }
-  
+
   var sequence = (function(){
     var s=2;
     return function(){
       return s++;
     }
   })()
-  
+
   function addSlide(){
     //query slide id
     var id,$step;
@@ -149,21 +149,21 @@ Builder=(function(){
     // jump to the overview slide to make some room to look around
     config['goto']('overview');
   }
-  
-  
+
+
   function downloadStyle(){
     var uriContent,content,$doc;
-    
+
     $.get('style.css', function (content) {
       var blob = new Blob([content], {type: "text/css;charset=utf-8"});
       saveAs(blob, "style.css");
     });
-   
+
   }
-  
+
   function downloadResults(){
     var uriContent,content,$doc;
-    
+
     $doc=$(document.documentElement).clone();
     //remove all scripting
     $doc.find('script').remove();
@@ -178,9 +178,9 @@ Builder=(function(){
     content=$doc[0].outerHTML;
     var blob = new Blob([content], {type: "text/html;charset=utf-8"});
     saveAs(blob, "presentation.html");
-      
+
   }
-  
+
   function editContents() {
     var $t = $(this);
     if(state.editing===true){
@@ -198,55 +198,55 @@ Builder=(function(){
       $t.after($txt.val(state.$node.html()));
     }
   }
-  
+
   function wrapContents() {
     state.$node.toggleClass('slide');
   }
-  
+
   function showControls($where){
     var top,left,pos=$where.offset();
     //not going out the edges (at least one way)
     top=(pos.top>0)? pos.top+(100/config.visualScaling) : 0;
     left=(pos.left>0)? pos.left+(100/config.visualScaling) : 0;
-    
+
     $controls.show().offset({
       top:top,
       left:left
     });
   }
-  
-  
+
+
   function loadData(){
     console.log('load',state.$node[0].dataset.x);
     //state.data=state.$node[0].dataset;
     //add defaults
-    
-    
-    state.data.x=parseFloat(state.$node[0].dataset.x) || defaults.x;   
-    state.data.y=parseFloat(state.$node[0].dataset.y) || defaults.y;   
-    state.data.scale=parseFloat(state.$node[0].dataset.scale) || defaults.scale;   
-    state.data.rotate=parseFloat(state.$node[0].dataset.rotate) || defaults.rotate;   
-    
+
+
+    state.data.x=parseFloat(state.$node[0].dataset.x) || defaults.x;
+    state.data.y=parseFloat(state.$node[0].dataset.y) || defaults.y;
+    state.data.scale=parseFloat(state.$node[0].dataset.scale) || defaults.scale;
+    state.data.rotate=parseFloat(state.$node[0].dataset.rotate) || defaults.rotate;
+
   }
-  
+
   function redraw(){
     clearTimeout(redrawTimeout);
     redrawTimeout=setTimeout(function(){
       //state.$node[0].dataset=state.data;
-        
+
       state.$node[0].dataset.scale=state.data.scale;
       state.$node[0].dataset.rotate=state.data.rotate;
       state.$node[0].dataset.x=state.data.x;
       state.$node[0].dataset.y=state.data.y;
       /**/
       console.log(state.data,state.$node[0].dataset,state.$node[0].dataset===state.data);
-        
+
       config.redrawFunction(state.$node[0]);
       showControls(state.$node);
     //console.log(['redrawn',state.$node[0].dataset]);
     },20);
   }
-  
+
   function fixVector(x,y){
     var result={x:0,y:0},
       angle=(config.rotation/180)*Math.PI,
@@ -257,25 +257,25 @@ Builder=(function(){
     result.y = (x*sn + y*cs) * config.visualScaling;
     return result;
   }
-  
+
   function handleMouseMove(e){
     e.preventDefault();
     e.stopPropagation();
-      
-      
+
+
     var x=e.pageX-mouse.prevX,
     y=e.pageY-mouse.prevY;
-        
+
     mouse.prevX=e.pageX;
     mouse.prevY=e.pageY;
     if(mouse.activeFunction){
       mouse.activeFunction(x,y);
       redraw();
     }
-    
+
     return false;
   }
-  
+
   return {
     init:init
   };
